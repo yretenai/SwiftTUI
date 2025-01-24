7ea62df937972cc9c3d215afd15eefb630df7af5
Ada <yretenai@gmail.com>
2025-01-24 02:05:40 +0000

    Output to preferred temp folders

--- Sources/SwiftTUI/Debug/log.swift
+++ Sources/SwiftTUI/Debug/log.swift
@@ -1,21 +1,30 @@
 import Foundation
 
-/// To enable logging, create an empty file under `/tmp/swift_tui_log`. Logging will append to the
+/// To enable logging, create an empty file under `$TMPDIR/swift_tui.log`. Logging will append to the
 /// end of this file.
 ///
 /// You can monitor the log live using:
 /// ```
-/// tail -f /tmp/swift_tui_log
+/// tail -f $TMPDIR/swift_tui.log
 /// ```
+///
+/// TMPDIR falls back to TMP then TEMP, otherwise C:\Users\(UserName)\AppData\Local\Temp on Windows or /tmp on *nix
 public func log(_ item: Any, terminator: String = "\n") {
     print(item, terminator: terminator, to: &logStream)
 }
 
-var logStream = LogStream()
+#if os(Windows)
+    let fallback = "C:\\Users\\\(ProcessInfo.processInfo.userName)\\AppData\\Local\\Temp"
+#else
+    let fallback = "/tmp"
+#endif
+let logPath = ProcessInfo.processInfo.environment["TMPDIR"] ?? ProcessInfo.processInfo.environment["TMP"] ?? ProcessInfo.processInfo.environment["TEMP"] ?? fallback
 
+let logURL = URL(fileURLWithPath: "\(logPath)/swift_tui\(ProcessInfo.processInfo.processIdentifier).log")
+
+var logStream = LogStream()
 struct LogStream: TextOutputStream {
     func write(_ string: String) {
-        let logURL = URL(fileURLWithPath: "/tmp/swift_tui_log")
         guard let data = string.data(using: .utf8) else {
             assertionFailure("Cannot write to log")
             return
